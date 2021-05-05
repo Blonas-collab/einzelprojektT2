8645c007a25633af66c76746b6631d2138607013
Jonas <jonasbloching@gmail.com>
2021-05-05 16:54:25 +0200

    wiederhergestelltes Zwischenergebnis

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -4,3 +4,4 @@ export const outputName = document.getElementById("outputName") as HTMLElement;
 export const nameInputWrapper = document.getElementById("nameInputWrapper") as HTMLDivElement;
 export const randomButton = document.getElementById("randomButton") as HTMLDivElement;
 export const result = document.getElementById("animalNameField") as HTMLElement;
+export let canvas = document.getElementById("canvas") as HTMLCanvasElement;
